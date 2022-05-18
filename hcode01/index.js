5e9535c043dde60ae3ec25969cd7bffa647012a3
Renan Perin Ribeiro <renan.perinribeiro@outlook.com>
2022-05-18 09:31:08 -0300

    update

---
+++ hcode01/index.js
@@ -0,0 +1,15 @@
+let cor = 'Azul';
+
+if (cor === 'Verde') {
+
+    console.log("Siga");
+
+} else if (cor === 'Amarelo') {
+
+    console.log("Atenção");
+    
+} else if (cor === "Vermelho") {
+
+    console.log("Pare");
+    
+}
---
+++ hcode01/index2.js
@@ -0,0 +1,20 @@
+let cor = "azul";
+
+switch (cor) {
+
+    case "verde":
+        console.log('Siga')
+        break
+    
+    case "amarelo":
+        console.log('Atenção')
+        break
+
+    case "Vermelho":
+        console.log('Pare')
+        break
+
+    default:
+        console.log("não sei")
+
+}
